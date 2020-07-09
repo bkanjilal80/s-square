b4edf72dad56756768fbd71298f944743f37ae0c
bkanjilal80 <58700422+bkanjilal80@users.noreply.github.com>
2020-07-09 16:21:40 +0530

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 15:30:23 2020
+
+@author: Bhaskar
+"""
+
+
+def main():
+    number = int(input('Enter the number (only positive integers allowed\n'))
+    print(f'{number} squares is {number**2}')
+
+
+if __name__ == '__main__':
+    main()
